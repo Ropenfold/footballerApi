a6e1c913ac0eba8a942f05b90e5889c5a81d81d0
ropenfold <stuartroper@gmail.com>
2019-04-09 14:01:57 +0100

    add additional route

--- routes/players.js
+++ routes/players.js
@@ -1,11 +1,39 @@
 var express = require("express");
 var router = express.Router();
-var Player = require("../models/players");
+var Player = require("../models/footballer");
 
 /* GET users listing. */
-router.get("/", async function(req, res, next) {
+router.get("/", async (req, res, next) => {
   const players = await Player.find({});
   res.json({ payload: players });
 });
+
+/* GET one player */
+router.get("/:id", async (req, res, next) => {
+  const id = Number(req.body.params);
+  const player = await Player.findOne({ ID: id });
+  res.json({ payload: player });
+});
+
+router.post("/", async (req, res, next) => {
+  const player = await new Player(req.body);
+  await player.save();
+  res.json({ playload: player });
+});
+
+router.patch("/:id", async (req, res, next) => {
+  const player = await Player.findOne({ ID: req.params.id });
+  const updates = Object.keys(req.body);
+  updates.forEach(update => {
+    player(update) = req.body(update);
+  });
+  const success = await player.save();
+  res.json({payload: success, message: `Updated ${req.param.id}`})
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const player = await Player.findOneAndRemove({ ID: req.params.id });
+  
+})
 
 module.exports = router;
